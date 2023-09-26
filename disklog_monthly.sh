e9e124dc52a8fe295cf8006fb4ead87854718b38
Danny <131976778+danitucek@users.noreply.github.com>
2023-09-26 16:34:29 +0200

    updated comments
    now including visualization of the timeformat output

--- disklog_monthly.sh
+++ disklog_monthly.sh
@@ -7,7 +7,7 @@ do
 	iostat_output="iostat_log_${month}"
 	dstat_output="dstat_log_${month}"
  
-# Put a formatted datestamp in both files on each iteration	
+# Put a formatted datestamp in both files on each iteration. The output format looks like this: 'January 1, 1970 12:34:56'.	
 	date '+%B %d, %Y %H:%M:%S' >> iostat_output
 	date '+%B %d, %Y %H:%M:%S' >> dstat_output
 	iostat -xy 1 1 | awk '/Device/ || /sda/ || /sdb/ {print}' >> $iostat_output
